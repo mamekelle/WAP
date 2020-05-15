e2c5def8e460ed6a635bf3209339f52748f3fc76
Mame <maman@edu.it>
2020-05-15 01:33:08 -0500

    :construction: LabW4D3

--- LabW4D3/parsingJson.js
+++ LabW4D3/parsingJson.js
@@ -1,7 +1,7 @@
 /*jshint  esversion:6, globalstrict:true */
 "use strict";
 $(function () {
-    $('#fetchUser').on('click', fetchUserJson);
+    $('#fetchUser').on('click', fetchUserJson1);
 
     function fetchUserJson() {
         const id = $('#userId').val();
@@ -19,6 +19,34 @@ $(function () {
             )
             .catch(error => console.error("Error: ", error));
         return html;
+
+    }
+
+    function fetchUserJson1() {
+        $.get('https://jsonplaceholder.typicode.com/users')
+            .done(ajaxSuccess)
+            .fail(alaxFail);
+    }
+
+    function ajaxSuccess(data) {
+        const id = $('#userId').val();
+        let html = $('#users').html("");
+        data.map(user => {
+            if (user.id === parseInt(id)) {
+                html.append("<p> ",`Id: ${user.id} `);
+                html.append("<p> ", `Name: ${user.name}`);
+                html.append("<p> ", `User name: ${user.username}`);
+                html.append("<p> ", `Emal: ${user.emal} `);
+                html.append("<p> ", `Phone: ${user.phone} `);
+                html.append("<p> ", `Website: ${user.website} `);
+            }
+        });
+        return html;
+
+    }
+
+    function alaxFail(xhr, status, exception) {
+        console.log(xhr, status, exception);
 
     }
 
